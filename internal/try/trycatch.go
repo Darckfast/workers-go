d0d3ddf7c1d5ee8d87088936aae059f0cd42a0a5
Victor Costa <21337578+Darckfast@users.noreply.github.com>
2025-08-17 15:22:26 -0300

    fix: fixed trycatch not loading on tests

--- internal/try/trycatch.go
+++ internal/try/trycatch.go
@@ -5,6 +5,34 @@ import (
 	"syscall/js"
 )
 
+func init() {
+	if !js.Global().Get("tryCatch").Truthy() {
+
+		fn := js.Global().Get("Function")
+
+		// Sync fn with JS error normalization: https://github.com/Darckfast/catch-this/blob/f96ceb6b7b5060281a152494353e07f9d6cbf0ca/index.js#L1
+		tryCatchFn := fn.New("fn", `{
+      try {
+      return {
+      result: fn(),
+      };
+      } catch (e) {
+      if (!(e instanceof Error)) {
+      if (e instanceof Object) {
+      e = JSON.stringify(e)
+      }
+      e = new Error(e || 'no error message')
+      }
+      return {
+      error: e,
+      };
+      }
+      }`)
+
+		js.Global().Set("tryCatch", tryCatchFn)
+	}
+}
+
 func TryCatch(fn js.Func) (js.Value, error) {
 	fnResultVal := js.Global().Call("tryCatch", fn)
 	resultVal := fnResultVal.Get("result")
