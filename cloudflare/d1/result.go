aab3e329a7af76fca0b56988d11425a04d3e7deb
Victor Costa <21337578+Darckfast@users.noreply.github.com>
2025-08-19 15:36:07 -0300

    fix: fixed int conversion and changes not returning

--- cloudflare/d1/result.go
+++ cloudflare/d1/result.go
@@ -6,6 +6,8 @@ import (
 	"database/sql"
 	"errors"
 	"syscall/js"
+
+	jsconv "github.com/Darckfast/workers-go/internal/conv"
 )
 
 type result struct {
@@ -16,17 +18,14 @@ var (
 	_ sql.Result = (*result)(nil)
 )
 
-// LastInsertId returns id of result's last row.
-// If lastRowId can't be retrieved, this method returns error.
 func (r *result) LastInsertId() (int64, error) {
 	v := r.resultObj.Get("meta").Get("last_row_id")
 	if v.IsNull() || v.IsUndefined() {
 		return 0, errors.New("d1: lastRowId cannot be retrieved")
 	}
-	id := v.Int()
-	return int64(id), nil
+	return jsconv.MaybeInt64(v), nil
 }
 
 func (r *result) RowsAffected() (int64, error) {
-	return int64(r.resultObj.Get("changes").Int()), nil
+	return jsconv.MaybeInt64(r.resultObj.Get("meta").Get("changes")), nil
 }
