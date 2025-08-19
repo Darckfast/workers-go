d0575c8d31ad9c41133fac19a0b912ea819a39f1
Victor Costa <21337578+Darckfast@users.noreply.github.com>
2025-08-19 11:46:35 -0300

    chore: moved types

--- worker/types/wasm.d.ts
+++ worker/types/wasm.d.ts
@@ -1,5 +1,7 @@
-declare module '*.wasm' {
-    interface WasmExports { }
-    const wasmModule: (imports?: WebAssembly.Imports) => Promise<{ instance: WebAssembly.Instance & { exports: WasmExports } }>;
-    export default wasmModule;
+declare module "*.wasm" {
+	interface WasmExports {}
+	const wasmModule: (
+		imports?: WebAssembly.Imports,
+	) => Promise<{ instance: WebAssembly.Instance & { exports: WasmExports } }>;
+	export default wasmModule;
 }
