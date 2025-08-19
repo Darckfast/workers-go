6bcdff408ab34a8b719680bb5cc657f3cceebf94
Victor Costa <21337578+Darckfast@users.noreply.github.com>
2025-08-19 11:46:53 -0300

    chore: formatted ts code

--- worker/durable_objects/go_container.ts
+++ worker/durable_objects/go_container.ts
@@ -4,18 +4,18 @@ import { Container } from "@cloudflare/containers";
  * its recommended to follow the official docs https://developers.cloudflare.com/containers
  */
 export class GoContainer extends Container {
-  defaultPort = 8080; // The default port for the container to listen on
-  sleepAfter = '6m'; // Sleep the container if no requests are made in this timeframe
+	defaultPort = 8080; // The default port for the container to listen on
+	sleepAfter = "6m"; // Sleep the container if no requests are made in this timeframe
 
-  override onStart() {
-    console.log('Container successfully started');
-  }
+	override onStart() {
+		console.log("Container successfully started");
+	}
 
-  override onStop() {
-    console.log('Container successfully shut down');
-  }
+	override onStop() {
+		console.log("Container successfully shut down");
+	}
 
-  override onError(error: unknown) {
-    console.log('Container error:', error);
-  }
+	override onError(error: unknown) {
+		console.log("Container error:", error);
+	}
 }
--- worker/main.ts
+++ worker/main.ts
@@ -1,17 +1,17 @@
-import { catchThis } from 'catch-this';
-import { connect } from 'cloudflare:sockets';
+import { connect } from "cloudflare:sockets";
+import { catchThis } from "catch-this";
 import app from "./bin/app.wasm";
 import "./bin/wasm_exec.js";
 
-export { GoContainer } from './durable_objects/go_container';
-export { TestDurableObject } from './durable_objects/test';
+export { GoContainer } from "./durable_objects/go_container";
+export { TestDurableObject } from "./durable_objects/test";
 
 /*
  * This import is only used with the sockets lib in Go
  */
 globalThis.cf = {
-  connect
-}
+	connect,
+};
 
 /**
  * A REQUIRED nice to have lib, since errors thrown within the JS runtime
@@ -20,47 +20,55 @@ globalThis.cf = {
  * This functions is just a try...catch with error normalization, that work both
  * with sync (callback) and async (promises) functions
  */
-globalThis.catchThis = catchThis.auto
+globalThis.catchThis = catchThis.auto;
 
 /**
-  * This function is what initialize your Go's compiled WASM binary
-  * only after this function has finished, that the handlers will be
-  * defined in the globalThis scope
-  *
-  * At the moment, due limitations with the getRandomData(), this block
-  * cannot be executed at top level, it must be contained within the handlers
-  * scope
-  *
-  * It's REQUIRED and needs to be called before using the globalThis.cf.<handler>()
-  */
+ * This function is what initialize your Go's compiled WASM binary
+ * only after this function has finished, that the handlers will be
+ * defined in the globalThis scope
+ *
+ * At the moment, due limitations with the getRandomData(), this block
+ * cannot be executed at top level, it must be contained within the handlers
+ * scope
+ *
+ * It's REQUIRED and needs to be called before using the globalThis.cf.<handler>()
+ */
 function init() {
-  const go = new Go()
-  go.run(new WebAssembly.Instance(app, go.importObject))
+	const go = new Go();
+	go.run(new WebAssembly.Instance(app, go.importObject));
 }
 
 async function fetch(req: Request, env: Env, ctx: ExecutionContext) {
-  init()
-  return await globalThis.cf.fetch(req, env, ctx);
+	init();
+	return await globalThis.cf.fetch(req, env, ctx);
 }
 
-async function email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext) {
-  init()
-  return await globalThis.cf.email(message, env, ctx)
+async function email(
+	message: ForwardableEmailMessage,
+	env: Env,
+	ctx: ExecutionContext,
+) {
+	init();
+	return await globalThis.cf.email(message, env, ctx);
 }
 
-async function scheduled(controler: ScheduledController, env: Env, ctx: ExecutionContext) {
-  init()
-  return await globalThis.cf.scheduled(controler, env, ctx)
+async function scheduled(
+	controler: ScheduledController,
+	env: Env,
+	ctx: ExecutionContext,
+) {
+	init();
+	return await globalThis.cf.scheduled(controler, env, ctx);
 }
 
 async function queue(batch: MessageBatch, env: Env, ctx: ExecutionContext) {
-  init()
-  return await globalThis.cf.queue(batch, env, ctx)
+	init();
+	return await globalThis.cf.queue(batch, env, ctx);
 }
 
 async function tail(events: TraceItem[], env: Env, ctx: ExecutionContext) {
-  init()
-  return await globalThis.cf.tail(events, env, ctx)
+	init();
+	return await globalThis.cf.tail(events, env, ctx);
 }
 
 /**
@@ -68,9 +76,9 @@ async function tail(events: TraceItem[], env: Env, ctx: ExecutionContext) {
  * only the ones that will be used
  */
 export default {
-  fetch,
-  email,
-  scheduled,
-  queue,
-  tail,
+	fetch,
+	email,
+	scheduled,
+	queue,
+	tail,
 } satisfies ExportedHandler<Env>;
