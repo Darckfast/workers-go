9c996bc9b2da2a1f13ecba2f24daaa733afdbddd
darckfast <21337578+Darckfast@users.noreply.github.com>
2025-08-27 09:57:30 -0300

    chore: implemented httprouter

--- worker/pkg/fetchhandler/routes.go
+++ worker/pkg/fetchhandler/routes.go
@@ -3,8 +3,6 @@
 package fetchhandler
 
 import (
-	"net/http"
-
 	httpcache "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/cache-api"
 	httpcontainer "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/container"
 	httpd1 "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/d1"
@@ -17,58 +15,60 @@ import (
 	httpr2 "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/r2"
 	httpsocket "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/sockets"
 	httptail "github.com/Darckfast/workers-go/worker/pkg/fetchhandler/tail"
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/Darckfast/workers-go/cloudflare/fetch"
 )
 
 func New() {
+	router := httprouter.New()
 	// HTTP
-	http.HandleFunc("GET /hello", httpsimple.GET_HELLO)
-	http.HandleFunc("GET /application/json", httpsimple.GET_JSON)
-	http.HandleFunc("POST /application/json", httpsimple.POST_JSON)
-	http.HandleFunc("POST /application/x-www-form-urlencoded", httpsimple.POST_FORM_URLENCODED)
-	http.HandleFunc("POST /multipart/form-data", httpsimple.POST_MULTIPART_FORM_DATA)
+	router.HandlerFunc("GET", "/hello", httpsimple.GET_HELLO)
+	router.HandlerFunc("GET", "/application/json", httpsimple.GET_JSON)
+	router.HandlerFunc("POST", "/application/json", httpsimple.POST_JSON)
+	router.HandlerFunc("POST", "/application/x-www-form-urlencoded", httpsimple.POST_FORM_URLENCODED)
+	router.HandlerFunc("POST", "/multipart/form-data", httpsimple.POST_MULTIPART_FORM_DATA)
 
-	// KV
-	http.HandleFunc("DELETE /kv", httpkv.DELETE_KV)
-	http.HandleFunc("POST /kv", httpkv.POST_KV)
-	http.HandleFunc("POST /kv/meta", httpkv.POST_KV_META)
-	http.HandleFunc("GET /kv/meta", httpkv.GET_KV_META)
-	http.HandleFunc("GET /kv", httpkv.GET_KV)
-	http.HandleFunc("GET /kvs", httpkv.GET_KVS)
-	http.HandleFunc("GET /kv/list", httpkv.GET_KV_LIST)
+	//", "KV
+	router.HandlerFunc("DELETE", "/kv", httpkv.DELETE_KV)
+	router.HandlerFunc("POST", "/kv", httpkv.POST_KV)
+	router.HandlerFunc("POST", "/kv/meta", httpkv.POST_KV_META)
+	router.HandlerFunc("GET", "/kv/meta", httpkv.GET_KV_META)
+	router.HandlerFunc("GET", "/kv", httpkv.GET_KV)
+	router.HandlerFunc("GET", "/kvs", httpkv.GET_KVS)
+	router.HandlerFunc("GET", "/kv/list", httpkv.GET_KV_LIST)
 
-	// R2
-	http.HandleFunc("GET /r2", httpr2.GET_R2)
-	http.HandleFunc("POST /r2", httpr2.POST_R2)
-	// Cache api
-	http.HandleFunc("GET /cache", httpcache.GET_CACHE)
-	// D1
-	http.HandleFunc("GET /d1", httpd1.GET_D1)
-	http.HandleFunc("POST /d1", httpd1.POST_D1)
-	http.HandleFunc("PUT /d1", httpd1.PUT_D1)
-	http.HandleFunc("DELETE /d1", httpd1.DELETE_D1)
-	http.HandleFunc("POST /d1/batch", httpd1.POST_D1_BATCH)
-	// Queue
-	http.HandleFunc("GET /queue", httpqueue.GET_QUEUE)
-	http.HandleFunc("POST /queue", httpqueue.POST_QUEUE)
-	// Socket
-	http.HandleFunc("GET /socket", httpsocket.GET_SOCKET_TCPBIN)
-	// Tail
-	http.HandleFunc("GET /tail", httptail.GET_TAIL)
-	// Container
-	http.HandleFunc("GET /container", httpcontainer.GET_CONTAINER)
-	// Durable Object
-	http.HandleFunc("GET /do", httpdurableobject.GET_DO)
-	// Env
-	http.HandleFunc("GET /env", httpenv.GET_ENV)
+	//", "R2
+	router.HandlerFunc("GET", "/r2", httpr2.GET_R2)
+	router.HandlerFunc("POST", "/r2", httpr2.POST_R2)
+	//", "Cache api
+	router.HandlerFunc("GET", "/cache", httpcache.GET_CACHE)
+	//", "D1
+	router.HandlerFunc("GET", "/d1", httpd1.GET_D1)
+	router.HandlerFunc("POST", "/d1", httpd1.POST_D1)
+	router.HandlerFunc("PUT", "/d1", httpd1.PUT_D1)
+	router.HandlerFunc("DELETE", "/d1", httpd1.DELETE_D1)
+	router.HandlerFunc("POST", "/d1/batch", httpd1.POST_D1_BATCH)
+	//", "Queue
+	router.HandlerFunc("GET", "/queue", httpqueue.GET_QUEUE)
+	router.HandlerFunc("POST", "/queue", httpqueue.POST_QUEUE)
+	//", "Socket
+	router.HandlerFunc("GET", "/socket", httpsocket.GET_SOCKET_TCPBIN)
+	//", "Tail
+	router.HandlerFunc("GET", "/tail", httptail.GET_TAIL)
+	//", "Container
+	router.HandlerFunc("GET", "/container", httpcontainer.GET_CONTAINER)
+	//", "Durable Object
+	router.HandlerFunc("GET", "/do", httpdurableobject.GET_DO)
+	//", "Env
+	router.HandlerFunc("GET", "/env", httpenv.GET_ENV)
 
 	//Error
-	http.HandleFunc("GET /error", errorshandler.GET_ERROR)
+	router.HandlerFunc("GET", "/error", errorshandler.GET_ERROR)
 
 	/*
 	 * Fetch handler uses http.DefaulServeMux as default, calling this function is
 	 * optional, unless a different handler must be used
 	 */
-	fetch.ServeNonBlock(nil)
+	fetch.ServeNonBlock(router)
 }
