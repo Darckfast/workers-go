ce9bf89558d7f301478de749b9b7f0a66d961b86
darckfast <21337578+Darckfast@users.noreply.github.com>
2025-08-28 21:29:14 -0300

    chore: updated config dir

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,35 +5,35 @@ import {
 import path from "path";
 
 export default defineWorkersConfig(async () => {
-	// Read all migrations in the `migrations` directory
-	const migrationsPath = path.join(__dirname, "./worker/migrations");
-	const migrations = await readD1Migrations(migrationsPath);
+  // Read all migrations in the `migrations` directory
+  const migrationsPath = path.join(__dirname, "./_worker/migrations");
+  const migrations = await readD1Migrations(migrationsPath);
 
-	return {
-		resolve: {
-			alias: {
-				$wrk: path.resolve(__dirname, "./worker"),
-			},
-		},
-		test: {
-			setupFiles: ["./tests/apply-migrations.ts"],
-			testTimeout: 10000,
-			poolOptions: {
-				workers: {
-					singleWorker: true,
-					wrangler: { configPath: "./worker/wrangler.toml" },
-					miniflare: {
-						kvNamespaces: ["TEST_NAMESPACE"],
-						bindings: { TEST_MIGRATIONS: migrations },
-						d1Databases: ["DB"],
-						r2Buckets: ["TEST_BUCKET"],
-						compatibilityFlags: ["service_binding_extra_handlers"],
-						queueConsumers: {
-							queue: { maxBatchTimeout: 0.05 /* 10ms */ },
-						},
-					},
-				},
-			},
-		},
-	};
+  return {
+    resolve: {
+      alias: {
+        $wrk: path.resolve(__dirname, "./_worker"),
+      },
+    },
+    test: {
+      setupFiles: ["./tests/apply-migrations.ts"],
+      testTimeout: 10000,
+      poolOptions: {
+        workers: {
+          singleWorker: true,
+          wrangler: { configPath: "./_worker/wrangler.toml" },
+          miniflare: {
+            kvNamespaces: ["TEST_NAMESPACE"],
+            bindings: { TEST_MIGRATIONS: migrations },
+            d1Databases: ["DB"],
+            r2Buckets: ["TEST_BUCKET"],
+            compatibilityFlags: ["service_binding_extra_handlers"],
+            queueConsumers: {
+              queue: { maxBatchTimeout: 0.05 /* 10ms */ },
+            },
+          },
+        },
+      },
+    },
+  };
 });
