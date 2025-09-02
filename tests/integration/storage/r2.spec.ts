d2cf40fee667c08238b4bf15dbf0cb085be749ff
darckfast <21337578+Darckfast@users.noreply.github.com.>
2025-09-02 08:51:16 -0300

    fix: fixed tests prop

--- tests/integration/storage/r2.spec.ts
+++ tests/integration/storage/r2.spec.ts
@@ -3,72 +3,72 @@ import { SELF } from "cloudflare:test";
 import { beforeAll, describe, expect, it, test } from "vitest";
 
 describe("r2", () => {
-	describe("write op", () => {
-		let rs: Response;
-		let params: URLSearchParams;
-		let body;
+  describe("write op", () => {
+    let rs: Response;
+    let params: URLSearchParams;
+    let body;
 
-		beforeAll(async () => {
-			params = new URLSearchParams();
+    beforeAll(async () => {
+      params = new URLSearchParams();
 
-			params.set("b64", faker.image.dataUri().split("base64,")[1]);
+      params.set("b64", faker.image.dataUri().split("base64,")[1]);
 
-			rs = await SELF.fetch("http://service/r2", {
-				method: "POST",
-				body: params,
-			});
+      rs = await SELF.fetch("http://service/r2", {
+        method: "POST",
+        body: params,
+      });
 
-			body = await rs.json();
-		});
+      body = await rs.json();
+    });
 
-		it("should have returned 200 status code", () => {
-			expect(rs.status).toBe(200);
-		});
+    it("should have returned 200 status code", () => {
+      expect(rs.status).toBe(200);
+    });
 
-		it('should have content-type "application/json"', () => {
-			expect(rs.headers.get("content-type")).toBe("application/json");
-		});
+    it('should have content-type "application/json"', () => {
+      expect(rs.headers.get("content-type")).toBe("application/json");
+    });
 
-		it("should have no error", async () => {
-			expect(body).toHaveProperty("has_error", false);
-		});
-		it("should have key count", async () => {
-			expect(body).toHaveProperty("result");
-			expect(body.result.Key).toBe("count");
-		});
-	});
+    it("should have no error", async () => {
+      expect(body).toHaveProperty("has_error", false);
+    });
+    it("should have key count", async () => {
+      expect(body).toHaveProperty("result");
+      expect(body.result.key).toBe("count");
+    });
+  });
 
-	describe("read op", () => {
-		let rs: Response;
-		let body;
-		let params;
+  describe("read op", () => {
+    let rs: Response;
+    let body;
+    let params;
 
-		beforeAll(async () => {
-			params = new URLSearchParams();
-			params.set("b64", btoa(faker.lorem.paragraph()));
+    beforeAll(async () => {
+      params = new URLSearchParams();
+      params.set("b64", btoa(faker.lorem.paragraph()));
 
-			await SELF.fetch("http://service/r2", {
-				method: "POST",
-				body: params,
-			});
+      await SELF.fetch("http://service/r2", {
+        method: "POST",
+        body: params,
+      });
 
-			rs = await SELF.fetch("http://service/r2", {
-				method: "GET",
-			});
+      rs = await SELF.fetch("http://service/r2", {
+        method: "GET",
+      });
 
-			body = await rs.json();
-		});
+      body = await rs.json();
+    });
 
-		it("should have returned 200 status code", () => {
-			expect(rs.status).toBe(200);
-		});
+    it("should have returned 200 status code", () => {
+      expect(rs.status).toBe(200);
+    });
 
-		it('should have content-type "application/json"', () => {
-			expect(rs.headers.get("content-type")).toBe("application/json");
-		});
+    it('should have content-type "application/json"', () => {
+      expect(rs.headers.get("content-type")).toBe("application/json");
+    });
 
-		it("should have body b64", async () => {
-			expect(body).toHaveProperty("body", params.get("b64"));
-		});
-	});
+    it("should have body b64", async () => {
+      expect(body).toHaveProperty("body", params.get("b64"));
+    });
+  });
 });
