ce7cd1600cc9c419c0fc4e9bb3c7c060c5e53035
darckfast <21337578+Darckfast@users.noreply.github.com.>
2025-09-07 20:16:38 -0300

    chore: updated types

--- _bun/types/global.d.ts
+++ _bun/types/global.d.ts
@@ -1,18 +1,6 @@
 declare global {
   var cf: {
-    fetch(r: Request, e?: Env | undefined, ctx?: ExecutionContext | undefined): Promise<Response>;
-    email(
-      m: ForwardableEmailMessage,
-      e: Env,
-      c: ExecutionContext,
-    ): Promise<void>;
-    scheduled(
-      c: ScheduledController,
-      e: Env,
-      ctx: ExecutionContext,
-    ): Promise<void>;
-    queue(b: MessageBatch, e: Env, ctx: ExecutionContext): Promise<void>;
-    tail(t: TraceItem[], e: Env, c: ExecutionContext): Promise<void>;
+    fetch(r: Request): Promise<Response>;
   };
 }
 export { };
