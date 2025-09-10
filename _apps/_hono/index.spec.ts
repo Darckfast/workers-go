9595b79b9ebf702d88941373449480428cc0c978
darckfast <21337578+Darckfast@users.noreply.github.com>
2025-09-09 21:42:37 -0300

    fix: fixed build:go cmd

--- _apps/_hono/index.spec.ts
+++ _apps/_hono/index.spec.ts
@@ -15,7 +15,7 @@ describe("GET /hello", () => {
   });
 
   it("should return hello from wasm", async () => {
-    const res = await fetch("http://localhost:5173/hello").catch(console.log);
+    const res = await fetch("http://localhost:5173/hello");
     const data = await res.text();
     assert.equal(res.status, 200);
     assert.equal(data, "hello");
