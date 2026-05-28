7ed78799a0008198f715d78451d0cb69a4c43076
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-05-28 12:16:01 -0300

    chore: updated deps and configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,39 +1,39 @@
 import {
-  defineWorkersConfig,
+  cloudflareTest,
   readD1Migrations,
-} from "@cloudflare/vitest-pool-workers/config";
-import path from "path";
+} from "@cloudflare/vitest-pool-workers";
+import path from "node:path";
+import { defineConfig } from "vitest/config";
 
-export default defineWorkersConfig(async () => {
-  // Read all migrations in the `migrations` directory
-  const migrationsPath = path.join(__dirname, "./_apps/_worker/migrations");
-  const migrations = await readD1Migrations(migrationsPath);
+export default defineConfig({
+  plugins: [
+    cloudflareTest(async () => {
+      const migrationsPath = path.join(__dirname, "./_apps/_worker/migrations");
+      const migrations = await readD1Migrations(migrationsPath);
 
-  return {
-    resolve: {
-      alias: {
-        $wrk: path.resolve(__dirname, "./_apps/_worker"),
-      },
-    },
-    test: {
-      setupFiles: ["./tests/apply-migrations.ts"],
-      testTimeout: 10000,
-      poolOptions: {
-        workers: {
-          singleWorker: true,
-          wrangler: { configPath: "./_apps/_worker/wrangler.toml" },
-          miniflare: {
-            kvNamespaces: ["TEST_NAMESPACE"],
-            bindings: { TEST_MIGRATIONS: migrations },
-            d1Databases: ["DB"],
-            r2Buckets: ["TEST_BUCKET"],
-            compatibilityFlags: ["service_binding_extra_handlers"],
-            queueConsumers: {
-              queue: { maxBatchTimeout: 0.05 /* 10ms */ },
-            },
+      return {
+        singleWorker: true,
+        wrangler: { configPath: "./_apps/_worker/wrangler.toml" },
+        miniflare: {
+          kvNamespaces: ["TEST_NAMESPACE"],
+          bindings: { TEST_MIGRATIONS: migrations },
+          d1Databases: ["DB"],
+          r2Buckets: ["TEST_BUCKET"],
+          compatibilityFlags: ["service_binding_extra_handlers"],
+          queueConsumers: {
+            queue: { maxBatchTimeout: 0.05 /* 10ms */ },
           },
         },
-      },
+      };
+    }),
+  ],
+  resolve: {
+    alias: {
+      $wrk: path.resolve(__dirname, "./_apps/_worker"),
     },
-  };
+  },
+  test: {
+    setupFiles: ["./tests/apply-migrations.ts"],
+    testTimeout: 10000,
+  },
 });
