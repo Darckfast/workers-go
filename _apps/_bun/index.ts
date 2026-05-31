ddf6eeac0b9d57a5d7b0d493ebd1fb6556e314ae
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-05-31 20:22:40 -0300

    fix: fixed js trycatch func not being created correctly

--- _apps/_bun/index.ts
+++ _apps/_bun/index.ts
@@ -1,5 +1,32 @@
 import "./bin/wasm_exec.js";
 
+/**
+ * A REQUIRED function, since errors thrown within the JS runtime
+ * inside Go's will cause the process to exit
+ *
+ * It's just a try...catch with error normalization
+ *
+ * This cannot be initialized within Go code, due Cloudflare workers
+ * limits
+ */
+globalThis.tryCatch = function(o, fn, args) {
+  try {
+    if (fn) {
+      return { data: o[fn](...args) };
+    }
+
+    return { data: o(...args) };
+  } catch (err) {
+    if (!(err instanceof Error)) {
+      if (err instanceof Object) {
+        err = JSON.stringify(err);
+      }
+      err = new Error(err || "no error message");
+    }
+    return { error: err };
+  }
+}
+
 const go = new Go();
 let initiliazed = false;
 const app = await Bun.file("./bin/app.wasm").arrayBuffer();
--- internal/try/trycatch.go
+++ internal/try/trycatch.go
@@ -6,30 +6,34 @@ import (
 	"syscall/js"
 )
 
-var catchThis js.Value
+var jsTryCatch js.Value = js.Global().Get("tryCatch")
 
 func init() {
-	catchThis = js.Global().Get("Function").New("o", "fn", "args", `{
-  try {
-    if (fn) {
-     return { data: o[fn](...args) };
-    }
+	if !jsTryCatch.Truthy() {
+		// Due Clouflare Workers limitation, this function cannot be
+		// instantiated within Go runtime
+		jsTryCatch = js.Global().Get("Function").New("o", "fn", "args", `{
+      try {
+      if (fn) {
+      return { data: o[fn](...args) };
+      }
 
-    return { data: o(...args) };
-  } catch (err) {
-    if (!(err instanceof Error)) {
+      return { data: o(...args) };
+      } catch (err) {
+      if (!(err instanceof Error)) {
       if (err instanceof Object) {
-        err = JSON.stringify(err);
+      err = JSON.stringify(err);
       }
       err = new Error(err || "no error message");
-    }
-    return { error: err };
-  }
-}`)
+      }
+      return { error: err };
+      }
+      }`)
+	}
 }
 
 func TryCatch(o js.Value, fn string, args ...any) (js.Value, error) {
-	fnResult := catchThis.Invoke(o, fn, args)
+	fnResult := jsTryCatch.Invoke(o, fn, args)
 	resultVal := fnResult.Get("data")
 	errorVal := fnResult.Get("error")
 
